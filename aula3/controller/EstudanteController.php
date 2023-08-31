4dc07cd205ff1ac83fb24737559a24b3a155b081
AlexandreBR <130804109+AlexandreBRN@users.noreply.github.com>
2023-08-31 20:53:50 -0300

    Update EstudanteController.php

--- aula3/controller/EstudanteController.php
+++ aula3/controller/EstudanteController.php
@@ -12,11 +12,17 @@ class EstudanteController{
     }
 
     public function salvar() {
-        $nome = $_GET['nome'];
-        $idade = $_GET['idade'];
-        $esdudanteModel = new EstudanteModel();
-        $esdudanteModel->salvarModel($nome, $idade);
+        if($_SERVER['REQUEST_METHOD'] == 'GET'){
+            Require_once $_SERVER['DOCUMENT_ROOT'] . '/aula3/view/EstudanteForm.php';
+        }elseif($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $nome = $_POST['nome'];
+            $idade = $_POST['idade'];
+            $esdudanteModel = new EstudanteModel();
+            $esdudanteModel->salvarModel($nome, $idade);
 
+            header('Location: http://localhost:8081/aula3/?controller=Estudante&acao=listar');
+            exit();
+        }
     }
 
 }
